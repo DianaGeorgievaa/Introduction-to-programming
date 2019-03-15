05fe1643cf2dde32d55593000a24835930b5f121
Diana Georgieva <40742204+DianaGeorgievaa@users.noreply.github.com>
2019-03-15 20:41:21 +0200

    Update Task 2.cpp

--- Homeworks/HomeWork1/Task 2.cpp
+++ Homeworks/HomeWork1/Task 2.cpp
@@ -4,23 +4,21 @@
 using  namespace  std;
 
 int main()
-{    // Reads a number
+{   
     int number;
     cout<<"Please enter a positive number"<<endl;
     cin>>number;
-    int new_num=0;
+    int newNumber=0;
     int n;
     do
     {
-        // Swaps the numbers
         n=number%10;
-        new_num=new_num*10+n;
+        newNumber=newNumber*10+n;
         number=number/10;
     }
     while(number>0);
-    // Converts to hex.
     {
-    cout<<hex<<new_num<<endl;
+    cout<<hex<<newNumber<<endl;
     }
 return 0;
 
